5cba5ab1e487a7eb7602bd5e2e51d0591d3df8a3
joshyeom <jhyeom95@daum.net>
2023-11-20 23:24:51 +0900

    refactor

--- src/Pages/QuotePage/Quote.jsx
+++ src/Pages/QuotePage/Quote.jsx
@@ -1,23 +1,15 @@
-import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as SC from './styled'
 
 const Quote = () => {
-  const [flag, setFlag] = useState(false);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (flag) {
-      navigate('/Earth');
-    }
-  }, [flag]);
-
-  const flagHandler = () => {
-    setFlag(true)
+  const handleRouter = () => {
+    navigate('/Earth');
   }
 
   return (
-    <SC.Main onAnimationEnd={flagHandler}>
+    <SC.Main onAnimationEnd={handleRouter}>
         <SC.Paragraph>
           “For my part I know nothing with any certainty, but the sight of the stars makes me dream.”
           <br />
